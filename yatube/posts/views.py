ce0e99e107137aca112c185a4d3bf569522c1abc
Dayan Iskhakov <dayan.iskhakoff@yandex.ru>
2022-04-18 10:30:05 +0500

    sprint6 end besides testing

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -31,7 +31,7 @@ def group_posts(request, slug):
 def new_post(request):
     content = {'title_name': 'Добавить запись', 'btn_name': 'Добавить'}
     if request.method == 'POST':
-        form = PostForm(request.POST)
+        form = PostForm(request.POST or None, files=request.FILES or None)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
@@ -40,47 +40,6 @@ def new_post(request):
         return render(request, 'new.html', {'form': form})
     form = PostForm()
     return render(request, 'new.html', {'form': form, 'content': content})
-
-
-def profile(request, username):
-    profile = get_object_or_404(User, username=username)
-    post_list = Post.objects.prefetch_related('author').filter(
-        author__exact=profile)
-    paginator = Paginator(post_list, 10)
-    page_number = request.GET.get('page')
-    page = paginator.get_page(page_number)
-    return render(request, 'profile.html', {'page': page, 'profile': profile,
-                                            'paginator': paginator})
-
-
-def post_view(request, username, post_id):
-    profile = get_object_or_404(User, username=username)
-    post = Post.objects.get(pk=post_id)
-    items = Comment.objects.filter(post_id=post_id).all()
-    flag = False  # флаг формы добавления комментария
-    return render(request, 'post.html', {'profile': profile, 'post': post,
-                                         'items': items, 'flag': flag})
-
-
-def add_comment(request, username, post_id):
-    post = Post.objects.get(pk=post_id)
-    items = Comment.objects.filter(post_id=post_id).all()
-    flag = True  # флаг формы добавления комментария
-    if request.method == 'POST':
-        form = CommentForm(request.POST or None)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.author = request.user
-            comment.post = post
-            comment.save()
-            return redirect(f'/{username}/{post_id}/')
-        return render(request, 'post.html', {'profile': post.author,
-                                             'post': post, 'form': form,
-                                             'flag': flag})
-    form = CommentForm()
-    return render(request, 'post.html', {'profile': post.author, 'post': post,
-                                         'form': form, 'items': items,
-                                         'flag': flag})
 
 
 @login_required
@@ -106,6 +65,52 @@ def post_edit(request, username, post_id):
                                         'post': post, 'profile': profile})
 
 
+def post_view(request, username, post_id):
+    profile = get_object_or_404(User, username=username)
+    post = Post.objects.get(pk=post_id)
+    items = Comment.objects.filter(post_id=post_id).all()
+    flag = False  # флаг формы добавления комментария
+    return render(request, 'post.html', {'profile': profile, 'post': post,
+                                         'items': items, 'flag': flag})
+
+
+def profile(request, username):
+    profile = get_object_or_404(User, username=username)
+    post_list = Post.objects.prefetch_related('author').filter(
+        author__exact=profile)
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    if Follow.objects.filter(author_id=profile.id, user_id=request.user.id):
+        following = True
+    else:
+        following = False
+    return render(request, 'profile.html', {'page': page, 'profile': profile,
+                                            'paginator': paginator,
+                                            'following': following})
+
+
+def add_comment(request, username, post_id):
+    post = Post.objects.get(pk=post_id)
+    items = Comment.objects.filter(post_id=post_id).all()
+    flag = True  # флаг формы добавления комментария
+    if request.method == 'POST':
+        form = CommentForm(request.POST or None)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            return redirect(f'/{username}/{post_id}/')
+        return render(request, 'post.html', {'profile': post.author,
+                                             'post': post, 'form': form,
+                                             'flag': flag})
+    form = CommentForm()
+    return render(request, 'post.html', {'profile': post.author, 'post': post,
+                                         'form': form, 'items': items,
+                                         'flag': flag})
+
+
 @login_required
 def follow_index(request):
     authors = User.objects.get(
@@ -120,16 +125,17 @@ def follow_index(request):
 
 @login_required
 def profile_follow(request, username):
-    user = User.objects.get(username=username)
-    if request.user != user:
-        Follow.objects.create(user_id=request.user.id, author_id=user.id)
+    author = User.objects.get(username=username)
+    if request.user != author:
+        Follow.objects.create(user_id=request.user.id, author_id=author.id)
     return redirect('profile', username=username)
 
 
 @login_required
 def profile_unfollow(request, username):
-    user = User.objects.get(username=username)
-    follow = Follow.objects.filter(user_id=request.user.id, author_id=user.id)
+    author = User.objects.get(username=username)
+    follow = Follow.objects.filter(user_id=request.user.id,
+                                   author_id=author.id)
     follow.delete()
     return redirect('profile', username=username)
 
